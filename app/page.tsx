e513061e3a70185b46e55c43b25768d5656ada96
Avinash Malhotra <avimalhotra505@gmail.com>
2026-03-28 12:43:07 +0530

    Quiz

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,50 @@
-import Image from "next/image";
+"use client";
+import data from "@/data/questions.json";
 
 export default function Home() {
+  
+    
+    let score=0;
+    
+    function checkAnswer(elem){
+        // console.log(elem.value);
+        // console.log( elem.getAttribute('data-ans'));
+
+        if( elem.value===elem.getAttribute('data-ans') ){ score++; }
+        console.log(score);
+    }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="container mx-auto p-3">
+      <header className="p-3">
+            <h1 className="text-5xl font-bold">Next JS Quiz</h1>
+      </header>
+      <main className="p-3">
+        <h2 className="text-3xl font-bold">Questions:</h2>
+        <p className="my-3">There are {data.length} questions in this quiz.</p>
+        {
+          data.map((elem)=>(
+              <div key={elem.id} className="border p-2">
+                <h3 className="text-3xl font-bold my-3">Question {elem.id} : <small>{elem.question}</small></h3>
+                <div className="my-3">
+                  <h4 className="text-2xl">Answers: Choose One</h4>
+                  <div>
+                    { elem.options.map((ques,ind)=>(
+                      <div key={ind} className="my-2">
+                        <label className="me-3" >
+                          <input type="radio" name={'Q'+elem.id} onChange={(e)=>checkAnswer(e.target)} value={ques} data-ans={elem.answer} /> : {ques} </label>
+                        </div>
+                    )) }
+                  </div>
+                </div>
+              </div>
+          ))
+        }
+
       </main>
+      <footer className="p-3 text-center">
+        <p>Site by: Avinash Malhotra</p>
+      </footer>
     </div>
   );
 }
